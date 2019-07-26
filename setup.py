7ba1375b33692dd0505873e49622e49bda089b29
adam cushner <adam.cushner@xaxis.com>
2019-07-26 18:39:30 -0400

    setup update

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name='misty_py',
-    version='0.9.0',
-    packages=['misty_py'],
+    version='0.9.2',
+    package_dir={'misty_py': 'misty_py',
+                 'misty_py.utils': 'misty_py/utils'},
+    packages=['misty_py', 'misty_py.utils'],
     url='https://github.com/acushner-xaxis/misty_py',
     license='MIT',
     author='adam cushner',
