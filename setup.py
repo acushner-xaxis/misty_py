e64783a1b282ab576aa64afd4fd64bfadfccf9b4
adam cushner <adam.cushner@xaxis.com>
2019-08-10 19:50:22 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='misty_py',
-    version='0.9.6',
+    version='0.9.7',
     packages=['misty_py', 'misty_py.utils'],
     package_dir={'misty_py': 'misty_py',
                  'misty_py.utils': 'misty_py/utils'},
